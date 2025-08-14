a017e79874eeef2ddf25a2ecc50bf6ecab861860
narmingasimli <nerminqasimli210@gmail.com>
2025-08-14 05:21:17 +0000

    add 0-lockboxes.py

---
+++ lockboxes/0-lockboxes.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def canUnlockAll(boxes):
+    n = len(boxes)
+    unlocked_boxes = {0}
+    keys_to_check = list(boxes[0])
+
+    while keys_to_check:
+        key = keys_to_check.pop(0)
+
+        if 0 <= key < n and key not in unlocked_boxes:
+            unlocked_boxes.add(key)
+            keys_to_check.extend(boxes[key])
+
+    return len(unlocked_boxes) == n
